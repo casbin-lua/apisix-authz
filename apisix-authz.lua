601f206ea23843877a6a6490af5ff3233b55fbf0
Rushikesh Tote <rushi.tote@gmail.com>
2021-07-24 22:51:33 +0530

    feat: Added core API functions
    Signed-off-by: Rushikesh Tote <rushi.tote@gmail.com>

--- apisix-authz.lua
+++ apisix-authz.lua
@@ -17,7 +17,7 @@ local core = require("apisix.core")
 local get_headers = ngx.req.get_headers
 
 -- e is the Casbin Enforcer
-local e
+local CasbinEnforcer
 
 local plugin_name = "apisix-authz"
 
@@ -42,21 +42,197 @@ local _M = {
 
 function _M.rewrite(conf)
     -- creates an enforcer when request sent for the first time
-    if not e then
-        e = Enforcer:new(conf.model_path, conf.policy_path)
+    if not CasbinEnforcer then
+        CasbinEnforcer = Enforcer:new(conf.model_path, conf.policy_path)
     end
 
     local path = ngx.var.request_uri
     local method = ngx.var.request_method
     local username = get_headers()[conf.username]
+    if not username then username = "anonymous" end
 
     if path and method and username then
-        if not e:enforce(username, path, method) then
+        if not CasbinEnforcer:enforce(username, path, method) then
             return 403, {message = "Access Denied"}
         end
     else
         return 403, {message = "Access Denied"}
     end
+end
+
+-- subject, object, action
+local function addPolicy()
+    local headers = get_headers()
+    local type = headers["type"]
+
+    if type == "p" then
+        local subject = headers["subject"]
+        local object = headers["object"]
+        local action = headers["action"]
+
+        if not subject or not object or not action then
+            return 400, {message = "Invalid policy request."}
+        end
+
+        if CasbinEnforcer:AddPolicy(subject, object, action) then
+            return 200, {message = "Successfully added policy."}
+        else
+            return 400, {message = "Invalid policy request."}
+        end
+    elseif type == "g" then
+        local user = headers["user"]
+        local role = headers["role"]
+
+        if not user or not role then
+            return 400, {message = "Invalid policy request."}
+        end
+
+        if CasbinEnforcer:AddGroupingPolicy(user, role) then
+            return 200, {message = "Successfully added grouping policy."}
+        else
+            return 400, {message = "Invalid policy request."}
+        end
+    else
+        return 400, {message = "Invalid policy type."}
+    end
+end
+
+local function removePolicy()
+    local headers = get_headers()
+    local type = headers["type"]
+
+    if type == "p" then
+        local subject = headers["subject"]
+        local object = headers["object"]
+        local action = headers["action"]
+
+        if not subject or not object or not action then
+            return 400, {message = "Invalid policy request."}
+        end
+
+        if CasbinEnforcer:RemovePolicy(subject, object, action) then
+            return 200, {message = "Successfully removed policy."}
+        else
+            return 400, {message = "Invalid policy request."}
+        end
+    elseif type == "g" then
+        local user = headers["user"]
+        local role = headers["role"]
+
+        if not user or not role then
+            return 400, {message = "Invalid policy request."}
+        end
+
+        if CasbinEnforcer:RemoveGroupingPolicy(user, role) then
+            return 200, {message = "Successfully removed grouping policy."}
+        else
+            return 400, {message = "Invalid policy request."}
+        end
+    else
+        return 400, {message = "Invalid policy type."}
+    end
+end
+
+-- subject, object, action
+local function hasPolicy()
+    local headers = get_headers()
+    local type = headers["type"]
+
+    if type == "p" then
+        local subject = headers["subject"]
+        local object = headers["object"]
+        local action = headers["action"]
+
+        if not subject or not object or not action then
+            return 400, {message = "Invalid policy request."}
+        end
+
+        if CasbinEnforcer:HasPolicy(subject, object, action) then
+            return 200, {data = "true"}
+        else
+            return 200, {data = "false"}
+        end
+    elseif type == "g" then
+        local user = headers["user"]
+        local role = headers["role"]
+
+        if not user or not role then
+            return 400, {message = "Invalid policy request."}
+        end
+
+        if CasbinEnforcer:HasGroupingPolicy(user, role) then
+            return 200, {data = "true"}
+        else
+            return 200, {data = "false"}
+        end
+    else
+        return 400, {message = "Invalid policy type."}
+    end
+end
+
+local function getPolicy()
+    local headers = get_headers()
+    local type = headers["type"]
+
+    if type == "p" then
+        local policy = CasbinEnforcer:GetPolicy()
+        if policy then
+            return 200, {data = policy}
+        else
+            return 400
+        end
+    elseif type == "g" then
+        local groupingPolicy = CasbinEnforcer:GetGroupingPolicy()
+        if groupingPolicy then
+            return 200, {data = groupingPolicy}
+        else
+            return 400
+        end
+    else
+        return 400, {message = "Invalid policy type."}
+    end
+end
+
+local function savePolicy()
+    local _, err = pcall(function ()
+        CasbinEnforcer:savePolicy()
+    end)
+    if not err then
+        return 200, {message = "Successfully saved policy."}
+    else
+        core.log.error("Save Policy error: " .. err)
+        return 400, {message = "Failed to save policy, see logs."}
+    end
+end
+
+function _M.api()
+    return {
+        {
+            methods = {"POST"},
+            uri = "/apisix/plugin/casbin/add",
+            handler = addPolicy,
+        },
+        {
+            methods = {"POST"},
+            uri = "/apisix/plugin/casbin/remove",
+            handler = removePolicy,
+        },
+        {
+            methods = {"GET"},
+            uri = "/apisix/plugin/casbin/has",
+            handler = hasPolicy,
+        },
+        {
+            methods = {"GET"},
+            uri = "/apisix/plugin/casbin/get",
+            handler = getPolicy,
+        },
+        {
+            methods = {"POST"},
+            uri = "/apisix/plugin/casbin/save",
+            handler = savePolicy,
+        },
+        }
 end
 
 function _M.check_schema(conf)
